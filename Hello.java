36879cede6e9107432d8028fd5412b863dcc1404
pradeeps2507 <63641008+pradeeps2507@users.noreply.github.com>
2020-06-01 23:10:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello 
 {
-public static void main(String[]args)
+public static void main(String[] args)
 
 {
 
-System.out.print("Hello World);
+System.out.println("Hello World");
 }
 
 }
